39171b94f42e7dcb363637120507b418bb3c9937
pohnerkartrushitaa <pohnerkar.trushitaa@gmail.com>
2026-06-16 00:12:26 +0530

    Add files via upload

---
+++ python programming/competitive/program1.py
@@ -0,0 +1,8 @@
+A = 67
+B = 90
+
+print("Value of a is: ",a)
+
+print(type(a))
+
+print(id(a))
---
+++ python programming/competitive/program2.py
@@ -0,0 +1,13 @@
+print("Enter value of 1st number :")
+a = int(input())
+
+print("Enter value of 2nd number :")
+b = int(input())
+
+print("Addition:",a+b)
+
+print("Subtraction:",a-b)
+
+print("Multiplication:",a*b)
+
+print("Division:",a/b)
