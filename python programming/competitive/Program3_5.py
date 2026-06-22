8983b4a6a96081e1d9347ec5a9d6bccacf5027e9
pohnerkartrushitaa <pohnerkar.trushitaa@gmail.com>
2026-06-22 21:45:09 +0530

    Add files via upload

---
+++ python programming/competitive/Program3_5.py
@@ -0,0 +1,15 @@
+def multiplication(No1,No2):
+    return No1*No2
+
+def main():
+    Value1=int(input("Enter a number:"))
+    Value2=int(input("Enter a numeber:"))
+
+    Ret=multiplication(Value1,Value2)
+
+    print("Multiplication:",Ret)
+
+
+if __name__=="__main__":
+    main()    
+
