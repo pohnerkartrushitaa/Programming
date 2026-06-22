c0b189cddf117fbab4696d1af92efbbbc7879cf4
pohnerkartrushitaa <pohnerkar.trushitaa@gmail.com>
2026-06-22 22:09:21 +0530

    Add files via upload

--- python programming/competitive/Program3_2.py
+++ python programming/competitive/Program3_2.py
@@ -4,8 +4,9 @@ def Main():
     print(type(X))
     print(id(X))
 
+    Y=input("Enter a value:")
     from sys import getsizeof
-    print(getsizeof(X))
-s
+    print(getsizeof(Y))
+
 if __name__=="__main__":
     Main()
